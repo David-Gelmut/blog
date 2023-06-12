3afd25b0155e0a7b49f05c063ddb9689ab0087d2
Gelmut David <david-gelmut@rambler.ru>
2023-06-12 15:06:27 +0500

    CRUD тэгов

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{route('admin.index')}}" class="brand-link">
         <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -13,6 +13,14 @@
                     <i class="nav-icon far fa-image"></i>
                     <p>
                         Категории
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="{{route('admin.tag.index')}}" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                      Тэги
                     </p>
                 </a>
             </li>
